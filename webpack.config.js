298701c88177a2f5fdebc8382f49da92532e3e9e
Hyeondoonge <migu554@naver.com>
2023-02-23 11:52:25 +0900

    :sparkles: html webpack plugin 추가 및 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Analogue Clock'
+    })
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
