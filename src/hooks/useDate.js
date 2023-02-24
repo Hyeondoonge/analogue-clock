5e1a16332581b66f8ddd3666eb56cecba69fbf2a
Hyeondoonge <migu554@naver.com>
2023-02-24 14:09:56 +0900

    :sparkles: 1초마다 date 상태 업데이트 역할하는 hook 추가

---
+++ src/hooks/useDate.js
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from 'react';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { dateValue, formatDateSelector } from '../recoil/time';
+
+export default function useDate() {
+  const setDate = useSetRecoilState(dateValue);
+  const interval = useRef(
+    setInterval(() => {
+      setDate(new Date());
+    }, 1000)
+  );
+  const formatDate = useRecoilValue(formatDateSelector);
+
+  useEffect(() => {
+    return () => {
+      clearInterval(interval.current);
+    };
+  }, []);
+
+  return formatDate;
+}
