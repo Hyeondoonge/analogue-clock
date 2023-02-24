71fce56513f95d1a308e49e55d0e33043c11a469
Hyeondoonge <migu554@naver.com>
2023-02-24 14:35:17 +0900

    :sparkles: track target 상태 추가

--- src/recoil/mouse.js
+++ src/recoil/mouse.js
@@ -1,8 +1,13 @@
 import { atom } from 'recoil';
 
+const trackTargetValue = atom({
+  key: 'trackTarget',
+  default: null
+});
+
 const mousePositionValue = atom({
   key: 'mouse',
   default: { isMouseOn: false, x: -1, y: -1 }
 });
 
-export { mousePositionValue };
+export { trackTargetValue, mousePositionValue };
